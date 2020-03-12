896b7e60c747bc31d9976b20505dbac7d3c73258
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-03-12 11:45:37 -0700

    Add documentation

--- R/sle.R
+++ R/sle.R
@@ -1,9 +1,26 @@
-#' Function to calculate the sea level rise equivalent of melting any given amount of ice
+#' Describes the equivalent change in sea level rise from ice volume
 #'
+#' Calculate the sea level rise equivalent (in mm) from melting any given volume of sea ice
+#' @param ice_vol a single numeric value sea ice volume in kilometers cubed
+#' @param above the percent of the sea ice that is above the surface of the water, in decimal format
 #'
+#' @return the sea level rise equivalent (in mm) caused by the melting of the input volume of sea ice (ice_vol)
+#'
+#' @example
+#' sle(ice_vol = 500, above = 0.6)
 
 
 sle <- function(ice_vol, above){
+
+  #Calculating based on one value for volume
+  if(length(ice_vol) > 1){
+    return("Provide a single numeric value for ice_vol, not a vector")
+  }
+
+  #Above input should be a decimal with a value between 0 and 1
+  if(above < 0 | above > 1){
+    return("Above parameter should be a percent in decimal format between 0 and 1")
+  }
 
   #only want to include volume of ice above the water that will melt from climate change
   #volume in km^3
