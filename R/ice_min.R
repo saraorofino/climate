1c45ee71cc11d9f78d2b8e12ff3959972ac2d993
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-03-11 20:26:29 -0700

    data link for ice min function

---
+++ R/ice_min.R
@@ -0,0 +1,4 @@
+#' Function to find the Arctic sea ice minimum, which occurs in September each year, and find a rate of decline
+#'
+#'  @data ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/north/monthly/data/
+
