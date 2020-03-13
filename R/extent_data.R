addbbbe850aa9edd8f86d68a59191de85bf3a826
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-03-12 17:12:06 -0700

    Documentation for data

---
+++ R/extent_data.R
@@ -0,0 +1,15 @@
+#' Test data for sea ice extent
+#'
+#' Sea ice extent (in square kilometers) each month from 1979-2019
+#'
+#'@format a data frame of 494 rows and 6 columns
+#'\describe{
+#'\item{year}{Year in four digit format}
+#'\item{mo}{Month in numeric format 1-12}
+#'\item{data-type}{Goddard: Sea Ice Concentrations from Nimbus-7 SMMR and DMSP SSM/I-SSMIS Passive Microwave Data}
+#'\item{region}{Hemisphere N = Northern}
+#'\item{extent}{Sea ice extent in millions of square kilometers}
+#'\item{area}{Sea ice area in millions of square kilometers}
+#'}
+#'
+'extent_data'
---
+++ R/sept_extent.R
@@ -0,0 +1,15 @@
+#' Monthly sea ice extent test data
+#'
+#' Sea ice extent for September (in square kilometers) from 1979-2019
+#'
+#'@format a data frame of 41 rows and 6 columns
+#'\describe{
+#'\item{year}{Year in four digit format}
+#'\item{mo}{Month in numeric format 1-12}
+#'\item{data-type}{Goddard: Sea Ice Concentrations from Nimbus-7 SMMR and DMSP SSM/I-SSMIS Passive Microwave Data}
+#'\item{region}{Hemisphere N = Northern}
+#'\item{extent}{Sea ice extent in millions of square kilometers}
+#'\item{area}{Sea ice area in millions of square kilometers}
+#'}
+#'
+'sept_extent'
