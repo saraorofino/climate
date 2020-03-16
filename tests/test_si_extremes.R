cd61f26dcba403c40fb6017c0b1c15dff682a86b
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-03-15 18:38:49 -0700

    create si_extremes test

---
+++ tests/test_si_extremes.R
@@ -0,0 +1,19 @@
+
+test_that("si_extremes_works", {
+
+  #Use the extent sample data:
+  data("extent_data")
+
+  #sea ice extent should be positive for both the max and the min
+  expect_true(si_extremes(extent_data)$max > 0)
+  expect_true(si_extremes(extent_data)$min > 0)
+
+  #sea ice extent maximum should be larger than the minimum
+  expect_true(si_extremes(sept_extent)$max > si_extremes(sept_extent)$min)
+
+
+})
+
+
+
+
