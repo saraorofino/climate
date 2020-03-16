3d67bf57d57d15c39b79bfa018e28534e8f0836b
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-03-15 19:13:01 -0700

    Testing for sle and si_extreme functions

--- tests/testthat/test_si_extremes.R
+++ tests/testthat/test_si_extremes.R
@@ -17,3 +17,4 @@ test_that("si_extremes_works", {
 
 
 
+
---
+++ tests/testthat/test_sle.R
@@ -0,0 +1,19 @@
+
+test_that("sle_works"){
+
+  #Create some test data with a series of ice volumes and percents:
+  ice_data <- data.frame(volume = c(500,200,600, 350, 465), #assign 5 volumes
+                         percent_above = c(0.5,0.2,0.6,0.8,1.2)) #assign 5 percents with one above 100
+
+
+  #The 'above' input should be a percent, for 1.2 it should return an error:
+  expect_equal(sle(ice_vol = ice_data$volume[2], above = ice_data$percent_above[5]),
+               "Above parameter should be a percent in decimal format between 0 and 1")
+
+  #The 'ice_vol' input can only accept a single value, if two or more values are inputted it should return an error:
+  expect_equal(sle(ice_vol = ice_data$volume[1:2], above = ice_data$percent_above[2]),
+               "Provide a single numeric value for ice_vol, not a vector")
+
+  #For a volume of 500 with 60% above water, the sle should be 0.7601161
+  expect_equal(sle(ice_vol = ice_data$volume[1], above = ice_data$percent_above[3]), 0.7601161)
+}
